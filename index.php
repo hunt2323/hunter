f5e52da1e9460959f085c01bb2adbca4ed8e14d2
hunt2323 <hunter2323@i.ua>
2015-09-29 22:18:39 +0300

    1

--- index.php
+++ index.php
@@ -1,13 +1,40 @@
 <?
 include 'config.php';
-function __autoload($class) {
-    include('lib/'.$class.'.php');
+function __autoload($class)
+{
+  include('lib/'.$class.'.php');
 }
+$sql_query='';
 
+<<<<<<< HEAD
+=======
+$row = array('list', 'list2');
+$table = array('table');
+$namedel= array('name');
+$valuedel = ('Vasya');
+$valueins = array('Kolya','Ivan');
+$oldName = array('Vasylyi');
+$newName = array('Alex');
+$limit ='';
+>>>>>>> hunt2323/master
 
-$sessid = 1;
-$sessvalue = 'Session test';
+$objMySql = new MySql;
+$sql_query.= $objMySql->selectQuery($row, $table, $limit).'<br>';
+$sql_query.= $objMySql->deleteQuery($table, $namedel, $valuedel, $limit).'<br>';
+$sql_query.= $objMySql->insertQuery($row, $valueins, $table, $limit).'<br>';
+$sql_query.= $objMySql->updateQuery($oldName,$newName,$table, $limit).'<br><hr>';
+$objPgSql = new PgSql;
+$sql_query.= $objPgSql->selectQuery($row, $table, $limit).'<br>';
+$sql_query.= $objPgSql->deleteQuery($table, $namedel, $valuedel, $limit).'<br>';
+$sql_query.= $objPgSql->insertQuery($row, $valueins, $table, $limit).'<br>';
+$sql_query.= $objPgSql->updateQuery($oldName,$newName,$table, $limit).'<br><hr>';
+$objMsSql = new MsSql;
+$sql_query.= $objMsSql->selectQuery($row, $table, '').'<br>';
+$sql_query.= $objMsSql->deleteQuery($table, $namedel, $valuedel, '').'<br>';
+$sql_query.= $objMsSql->insertQuery($row, $valueins, $table, '').'<br>';
+$sql_query.= $objMsSql->updateQuery($oldName,$newName,$table, '').'<br><hr>';
 
+<<<<<<< HEAD
 $objSession = new Session;
 $value = '';
 $value1 = '';
@@ -23,4 +50,7 @@ else($value1 == false){
 
 
 include TEMPL;
+=======
+include VIEW;
+>>>>>>> hunt2323/master
 ?>
--- templates/index.php
+++ templates/index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Interface</title>
+    <title>SQL query</title>
 </head>
 <body>
+<<<<<<< HEAD
     <h3>Interface</h3>
 <?
 =$value1;
 =$value;
 =$value1;
     ?>
+=======
+    <h3>SQL query</h3>
+<?=$sql_query?>
+>>>>>>> hunt2323/master
 
 </body>
 </html>
